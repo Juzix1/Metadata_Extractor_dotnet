9848cde2d1d85334a048150bf2ed579381e29852
Juzix1 <58827642+Juzix1@users.noreply.github.com>
2024-12-03 12:04:03 +0100

    minor fixes

--- CONSOLE/Program.cs
+++ CONSOLE/Program.cs
@@ -18,51 +18,56 @@ namespace ConsoleApp
 {
     class Program {
         private readonly ILogger _logger;
-
+        
         public static async Task<int> Main(string[] args) {
+            while (true) {
 
-            var client = new MetadataServiceClient(MetadataServiceClient.EndpointConfiguration.WSHttpBinding_IMetadataService, "https://localhost:5001/MetadataService/WSHttps");
-            string dllPath = @"C:\Users\kapik\Documents\Visual Studio 2022\MetaDataExtractor\Metadata_Extractor_dotnet\CONSOLE\bin\Debug\net6.0\Model.dll";
+                Console.WriteLine("====================");
+                Console.WriteLine("\tConsole DDL Extractor");
+                Console.WriteLine("====================");
+                Console.WriteLine("MENU\n1.Server Side.\n2.Locally");
+                string choose = Console.ReadLine();
+                switch (choose) {
 
-            if (!File.Exists(dllPath)) {
-                Console.WriteLine($"File {dllPath} not found.");
-                return 0;
+                    case "2":
+                        Console.Clear();
+                        
+                        Console.WriteLine("Starting DLL Analyzer in Console Mode...");
+                        Console.WriteLine("Write dll file path:");
+                        string path = Console.ReadLine();
+                        await showMetaData(path);
+                            
+                            break;
+                    case "1":
+                        Console.Clear();
+                        var client = new MetadataServiceClient(MetadataServiceClient.EndpointConfiguration.WSHttpBinding_IMetadataService, "https://localhost:5001/MetadataService/WSHttps");
+                        Console.WriteLine("Write dll file path:");
+                        string dllPath = Console.ReadLine();
+
+                        if (!File.Exists(dllPath)) {
+                            Console.WriteLine($"File {dllPath} not found.");
+                            break;
+                        }
+
+                        byte[] fileBytes = File.ReadAllBytes(dllPath);
+
+                        try {
+
+                            string result = await client.ProcessMetadataResultAsync(fileBytes);
+                            Console.WriteLine($"Processed Metadata: {result}");
+                        } catch (Exception ex) {
+                            Console.WriteLine($"Error: {ex.Message}");
+                        }
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid Choice! Try Again.");
+                        break;
             }
 
-            byte[] fileBytes = File.ReadAllBytes(dllPath);
 
-            try {
-                string result = await client.ProcessMetadataResultAsync(fileBytes);
-                Console.WriteLine($"Processed Metadata: {result}");
-            } catch (Exception ex) {
-                Console.WriteLine($"Error: {ex.Message}");
             }
-        
-    
 
-
-
-            //var client = new EchoServiceClient(EchoServiceClient.EndpointConfiguration.WSHttpBinding_IEchoService, "https://localhost:5001/EchoService/WSHttps");
-
-            //var simpleResult = await client.EchoAsync("Hello");
-            //Console.WriteLine(simpleResult);
-
-            //var msg = new EchoMessage() { Text = "Hello2" };
-            //var msgResult = await client.ComplexEchoAsync(msg);
-            //Console.WriteLine(msgResult);
-            //return 0;
-            return 0;
-        }/*
-        
-        public static int Main(string[] args) {
-            if (args.Length > 0) {
-                Console.WriteLine("Starting DLL Analyzer in Console Mode...");
-                showMetaData(args[0]);
-                return 0;
-            } else {
-                Console.WriteLine("Invalid File path, try again");
-                return 0;
-            }
 
 
         }
@@ -74,7 +79,9 @@ namespace ConsoleApp
             try {
                 MetadataExtractor metaData = new MetadataExtractor(logger);
                 AssemblyInfo assemblyInfo = await metaData.ExtractMetadataAsync(filePath);
-                
+
+                if (assemblyInfo.Types.Count == 0)
+                    throw new Exception("File is Empty");
                 foreach (var type in assemblyInfo.Types) {
                     Console.WriteLine($"Type: {type.TypeName}");
                     foreach (var method in type.Methods) {
@@ -109,10 +116,10 @@ namespace ConsoleApp
 
             } catch (Exception se) {
                 await logger.LogErrorAsync();
-                Console.WriteLine($"Error in reading the dll file {se.Message}");
+                Console.WriteLine($"Error in reading the dll. {se.Message}");
             }
 
-        }*/
+        }
 
     }
         
